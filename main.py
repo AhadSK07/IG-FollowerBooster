1d90fc2c7c5489a0f5ddb7383bfddc26690e27f1
ahadk69 <121217997+ahadk69@users.noreply.github.com>
2023-02-10 19:21:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import json
 from followers import SendFollowers
 from pwd_change import ChangePassword
 
-urls = ["https://freefollowerx.com/login",
-        "https://instamoda.org/login",
-        "https://takipcitime.com/login",
-        "https://takipcikrali.com/login",
-        "https://takipcimx.net/login",
-        "https://takipciking.com/member",
-        "https://birtakipci.com/member",
-        "https://medyahizmeti.com/member"]
+URLS = [
+    "https://freefollowerx.com/login",
+    "https://instamoda.org/login",
+    "https://takipcitime.com/login",
+    "https://takipcikrali.com/login",
+    "https://takipcimx.net/login",
+    "https://takipciking.com/member",
+    "https://birtakipci.com/member",
+    "https://medyahizmeti.com/member",
+]
 
 with open("accounts.json", "r") as file:
     accounts = json.load(file)
-    for account in accounts.items():
-        print(f"Account: {account[0]}")
-        for url in urls:
-            SendFollowers(url, account[0], account[1], "ah4d1337")
-        ChangePassword(account[0], account[1], accounts)
+    for account_name, account_password in accounts.items():
+        print(f"Account: {account_name}")
+        for url in URLS:
+            SendFollowers(url, account_name, account_password, "ah4d1337")
+        ChangePassword(account_name, account_password, accounts)
