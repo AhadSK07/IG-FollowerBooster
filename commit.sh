6e3264e7989e495ec42447f73371742dc918850c
ahadk69 <121217997+ahadk69@users.noreply.github.com>
2023-02-12 02:04:30 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,6 +3,6 @@ git config --global user.email $GH_MAIL
 git clone https://github.com/$GITHUB_REPOSITORY repo
 mv config.json repo/config.json
 cd repo || exit 1
-git add accounts.json
+git add config.json
 git commit -m "Update Accounts Password"
 git push -q https://$GH_TOKEN@github.com/$GITHUB_REPOSITORY HEAD:main
