4decdc2b0967b3cc683d0e0ef97f60f131808131
ahadk69 <121217997+ahadk69@users.noreply.github.com>
2023-02-11 18:13:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,32 @@ URLS = [
     "https://medyahizmeti.com/member",
 ]
 
-with open("config.json", "r") as file:
-    accounts = json.load(file)
-    for account_name, account_password in accounts.items():
-        print(f"Account: {account_name}")
-        for url in URLS:
-            FollowersSender(url, account_name, account_password, "ahad.le3t").run()
+def process_account(urls, account_name, account_password, target_username):
+    for url in urls:
+        try:
+            FollowersSender(url, account_name, account_password, target_username).run()
+        except Exception as e:
+            print(f"Failed to send followers with error: {str(e)}")
+    try:
         PasswordChanger(account_name, account_password, accounts).run()
+    except Exception as e:
+        print(f"Failed to change password with error: {str(e)}")
+
+def get_accounts(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"The file {file_path} does not exist.")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Failed to parse the JSON data from {file_path}.")
+        return {}
+
+def main()
+    accounts = get_accounts("config.json")
+    for account_name, account_password in accounts.items():
+        print(f"Processing account: {account_name}")
+        process_account(URLS, account_name, account_password, "ahad.le3t")
+
+main()
