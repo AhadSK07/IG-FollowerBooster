db3019ba8ce775fb38f7f02d7f34a9dc9ea87173
ahadk69 <121217997+ahadk69@users.noreply.github.com>
2023-03-12 10:49:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ def process_account(urls, account_name, account_password, accounts, target_usern
             FollowersSender(url, account_name, account_password, target_username).run()
         except Exception as e:
             print(f"Failed to send followers with error: {str(e)}")
+"""
     try:
         PasswordChanger(account_name, account_password, accounts).run()
     except Exception as e:
         print(f"Failed to change password with error: {str(e)}")
+"""
 
 def get_accounts(file_path):
     try:
